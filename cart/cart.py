72f2535c89a97e492f44e99d239784435b7579b5
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-18 18:03:23 +0000

    Full shopping cart template

--- cart/cart.py
+++ cart/cart.py
@@ -8,7 +8,7 @@ class Cart(object):
 
     def __init__(self, request):
         """Initialize the cart"""
-        self.session = request.sessions
+        self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             # save an empty cart in the session
