7eaa49ea53e43baa3b9614242f857b33a9b0ba1c
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-20 17:41:47 +0000

    Asynchronous order confirmation messages with Celery and RabbitMQ

--- orders/tasks.py
+++ orders/tasks.py
@@ -11,7 +11,7 @@ def order_created(order_id):
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name}, \n\n' \
         f'You have successfully placed an order.' \
-        f'Your order ID is {ored.id}'
+        f'Your order ID is {order.id}'
     mail_sent = send_mail(subject,
                           message,
                           'admin@myshop.com',
