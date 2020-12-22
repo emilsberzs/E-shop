614096e936b16adadf3d8b1df837e862099c4c21
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-22 20:23:50 +0000

    CouponApply form, modified urls to route coupons, created views so coupon works in shoping cart

--- myshop/urls.py
+++ myshop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
